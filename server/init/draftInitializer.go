822a7e5df61892fc729c6e38592beb16f0c07b6b
squirlemaster42 <jakobmisbach8@gmail.com>
2024-03-02 11:47:43 -0500

    Created function to init a draft from a csv

--- server/init/draftInitializer.go
+++ server/init/draftInitializer.go
@@ -1,13 +1,54 @@
 package init
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"server/database"
 	"strings"
 )
 
-func loadCSVIntoDb(dbHandle *database.DatabaseDriver, filepath string) {
+func loadCSVIntoDb(dbHandler *database.DatabaseDriver, path string) {
     //Load a csv at a give filepath
     //Add the relevant data to the database
+    lines := loadFile(path)
+    players := parseCSVToPlayers(lines)
+    draftName := filepath.Base(path)
+
+    //Delete old data related to this draft if it exists
+    draftId := getDraftId(draftName, dbHandler)
+    dbHandler.RunExec(fmt.Sprintf("Delete From Picks Where draftId = %d", draftId))
+
+    //Add players
+    for player := range players {
+        dbHandler.RunExec(fmt.Sprintf("Insert Into Players (NAME) Values (%s)", player))
+    }
+
+    //Add picks for the players
+    for player, picks := range players {
+        playerId := getPlayerId(player, dbHandler)
+        for index, pick := range picks {
+            dbHandler.RunExec(fmt.Sprintf("INSERT INTO Picks (draftId, player, PickOrder, pickedTeam) VALUES (%d, %d, %d, %s)", draftId, playerId, index, pick))
+        }
+    }
+}
+
+func getDraftId(draftName string, dbHandler *database.DatabaseDriver) int {
+    var draftId int
+    err := dbHandler.Connection.QueryRow(fmt.Sprintf("Select Id From drafts Where Name = %s", draftName), 1).Scan(&draftId)
+    if err != nil {
+        return 0
+    }
+    return draftId
+}
+
+func getPlayerId(playerName string, dbHandler *database.DatabaseDriver) int {
+    var playerId int
+    err := dbHandler.Connection.QueryRow(fmt.Sprintf("Select Id From Players Where Name = %s", playerName), 1).Scan(&playerId)
+    if err != nil {
+        return 0
+    }
+    return playerId
 }
 
 func parseCSVToPlayers(lines []string) map[string][]string {
@@ -21,11 +62,23 @@ func parseCSVToPlayers(lines []string) map[string][]string {
 
     for i := 0; i < teamsPerPlayer; i++ {
         line := lines[i + 1]
-        plitLine := strings.Split(line, ",")
+        splitLine := strings.Split(line, ",")
         for j := 0; j < len(splitLine); j++ {
-            teamsPerPlayer[playerNames[j]] = append(teamsPerPlayer[playerNames[j], splitLine[j])
+            p := playerNames[j]
+            teamsForPlayers[p] = append(teamsForPlayers[p], "tba" + splitLine[j])
         }
     }
 
     return teamsForPlayers
+}
+
+func loadFile(filePath string) []string {
+    body, err := os.ReadFile(filePath)
+    if err != nil {
+        return nil
+    }
+
+    lines := strings.Split(string(body), "\n")
+
+    return lines
 }
