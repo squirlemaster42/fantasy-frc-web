a161e0d7ce9b10afac56e5e6d45d48285612ce10
squirlemaster42 <jakobmisbach8@gmail.com>
2024-03-17 22:27:22 -0400

    Started team model

---
+++ server/web/model/team.go
@@ -0,0 +1,40 @@
+package model
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"server/database"
+)
+
+type Team struct {
+    tbaId string
+    name string
+    rankingScore int
+    validPick bool
+}
+
+func upsertTeam(team *Team, dbDriver *database.DatabaseDriver) {
+	query := fmt.Sprintf(`INSERT INTO Teams (tbaId, name, rankingScore, validPick)
+    VALUES ('%s', '%s', %d, %t)
+    ON CONFLICT(tbaId)
+    DO UPDATE SET
+    name = EXCLUDED.name, rankingScore = EXCLUDED.rankingScore;`, team.tbaId, html.EscapeString(team.name), team.rankingScore, team.validPick)
+    dbDriver.RunExec(query)
+}
+
+func getTeam(tbaId string, dbDriver *database.DatabaseDriver) *Team {
+    query := `SELECT tbaId, name, rankingScore, validPick FROM Teams WHERE tbaId = $1`
+    stmt, err := dbDriver.Connection.Prepare(query)
+
+    if err != nil {
+        log.Println(err)
+        return &Team{}
+    }
+
+    var team Team
+
+    stmt.QueryRow(tbaId).Scan(&team.tbaId, &team.name, &team.rankingScore, &team.validPick)
+
+    return &team
+}
