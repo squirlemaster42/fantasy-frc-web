ca2e012acda9ced1eb9ab9e8b49b7110fd78019d
squirlemaster42 <jakobmisbach8@gmail.com>
2024-03-20 21:59:15 -0400

    Started find next pick

--- server/web/model/pick.go
+++ server/web/model/pick.go
@@ -13,6 +13,22 @@ type Pick struct {
     PickedTeam int
 }
 
+func GetNextPlayerPickOrder(draftId int, dbDriver *database.DatabaseDriver) (int, int) {
+    query := `Select PlayerOrder, PickOrder From Picks
+    Inner Join (Select Max(Id) As Id From Picks Where draftId = $1) m On m.Id = Picks.Id;`
+    stmt, err := dbDriver.Connection.Prepare(query)
+
+    if err != nil {
+        log.Println(err)
+    }
+
+    var playerOrder int
+    var pickOrder int
+    err = stmt.QueryRow(draftId).Scan(&playerOrder, &pickOrder)
+
+    return 0, 0
+}
+
 func MakePick(playerId int, pickOrder int, playerOrder int, draftId int, pickedTeam string, dbDriver *database.DatabaseDriver) {
     query := `INSERT INTO Picks (playerId, pickOrder, playerOrder, draftId, pickedTeam)
     VALUES ($1, $2, $3, $4, $5)
@@ -32,4 +48,22 @@ func MakePick(playerId int, pickOrder int, playerOrder int, draftId int, pickedT
     if err != nil {
         log.Println(err)
     }
+}
+
+func GetPick(playerId int, draftId int, dbDriver *database.DatabaseDriver) string {
+    query := `Select pickedTeam From Picks Where playerId = $1 AND draftId = $2`
+    stmt, err := dbDriver.Connection.Prepare(query)
+
+    var pick string
+    if err != nil {
+        log.Println(err)
+    }
+
+    err = stmt.QueryRow(playerId, draftId).Scan(&pick)
+
+    if err != nil {
+        log.Println(err)
+    }
+
+    return pick
 }
