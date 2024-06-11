bbf0451a45ff82ce9c8869bf7838820af3cc7e8a
squirlemaster42 <jakobmisbach8@gmail.com>
2024-06-10 22:12:41 -0400

    Feat: User Crud

--- server/model/user.go
+++ server/model/user.go
@@ -1,6 +1,9 @@
 package model
 
-import "database/sql"
+import (
+	"database/sql"
+	"server/assert"
+)
 
 //TODO The primary key here is not good. I should fix that.
 type User struct {
@@ -9,18 +12,53 @@ type User struct {
     Password string
 }
 
-func RegisterUser(database *sql.DB, user User) error {
-    return nil
+func RegisterUser(database *sql.DB, username string, password string) {
+    query := `INSERT INTO Users (username, password) Value($1, $2);`
+    assert := assert.CreateAssertWithContext("Register User")
+    assert.AddContext("Username", username)
+    assert.AddContext("Password", password)
+    stmt, err := database.Prepare(query)
+    assert.NoError(err, "Failed to prepare statement")
+    _, err = stmt.Exec(username, password)
+    assert.NoError(err, "Failed to register user")
 }
 
 func UsernameTaken(database *sql.DB, username string) bool {
-    return false
+    query := `Select count(Id) From Users Where username = $1;`
+    assert := assert.CreateAssertWithContext("Username Taken")
+    assert.AddContext("Username", username)
+    stmt, err := database.Prepare(query)
+    assert.NoError(err, "Failed to prepare statement")
+    var count int
+    err = stmt.QueryRow(username).Scan(&count)
+    assert.NoError(err, "Failed to check username taken")
+    return count > 0
 }
 
+//All crypto should happen before this since this just communicates with the DB
+//TODO Should the crypto be rolled into this?
 func ValidateLogin(database *sql.DB, username string, password string) bool {
-    return false
+    query := `Select password From Users Where username = $1;`
+    assert := assert.CreateAssertWithContext("Validate Login")
+    assert.AddContext("Username", username)
+    assert.AddContext("Password", password)
+    stmt, err := database.Prepare(query)
+    assert.NoError(err, "Failed to prepare statement")
+    var dbPassword string
+    err = stmt.QueryRow(username).Scan(&dbPassword)
+    assert.NoError(err, "Failed to validate login")
+    return dbPassword == password
 }
 
-func UpdatePassword(database *sql.DB, username string, oldPassword string, newPassword string) bool {
-    return false
+//The old password logic should happen before this
+//TODO Should we move more logic here?
+func UpdatePassword(database *sql.DB, username string, newPassword string) {
+    query := `Update Users Set password = $1 Where username = $2;`
+    assert := assert.CreateAssertWithContext("Update Password")
+    assert.AddContext("Username", username)
+    assert.AddContext("New Password", newPassword)
+    stmt, err := database.Prepare(query)
+    assert.NoError(err, "Failed to prepare statement")
+    _, err = stmt.Exec(newPassword, username)
+    assert.NoError(err, "Failed to Update Password")
 }
