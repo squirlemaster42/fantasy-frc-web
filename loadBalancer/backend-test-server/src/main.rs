8cfd0b6efca00c61c5a723e8bce975c780fe0a97
squirlemaster42 <jakobmisbach8@gmail.com>
2024-06-20 21:54:57 -0400

    Added Test Backend Server

--- loadBalancer/backend-test-server/src/main.rs
+++ loadBalancer/backend-test-server/src/main.rs
@@ -6,7 +6,7 @@ use std::{
 fn main() {
     println!("-------- Starting Load Balancer --------");
 
-    let listener = TcpListener::bind("localhost:4000").unwrap();
+    let listener = TcpListener::bind("localhost:3030").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -15,9 +15,6 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    //We need to open a connection to the new server and forward the response
-    //TODO This needs to be multi-threaded
-
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
         .lines()
@@ -28,7 +25,7 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {http_request:#?}");
 
     let status = "HTTP/1.1 200 OK";
-    let content = "Hello from the server\n";
+    let content = "Hello from the backend server\n";
     let length = content.len();
     let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
     stream.write_all(response.as_bytes()).unwrap();
