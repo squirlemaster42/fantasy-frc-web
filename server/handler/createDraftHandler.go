6a2757f845332fe7d52a8d0fdaebed0eecc80667
squirlemaster42 <jakobmisbach8@gmail.com>
2024-07-15 23:49:44 -0400

    finished create draft post

--- server/handler/createDraftHandler.go
+++ server/handler/createDraftHandler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
-	"fmt"
 	"server/assert"
+	"server/model"
 	"server/view/draft"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -23,17 +25,35 @@ func (h *Handler) HandleCreateDraftPost(c echo.Context) error {
     interval := c.FormValue("interval")
     startTime := c.FormValue("startTime")
     endTime := c.FormValue("endTime")
+    sessionToken, err := c.Cookie("SessionToken")
+    assert := assert.CreateAssertWithContext("Handle Create Draft Post")
 
-    fmt.Println(draftName)
-    fmt.Println(description)
-    fmt.Println(interval)
-    fmt.Println(startTime)
-    fmt.Println(endTime)
+    intInterval, err := strconv.Atoi(interval)
+    assert.NoError(err, "Failed to parse interval")
+
+    layout := "2006-01-02 155:04:05"
+    parsedStartTime, err := time.Parse(layout, startTime)
+    assert.NoError(err, "Failed to parse start time")
+    parsedEndTime, err := time.Parse(layout, endTime)
+    assert.NoError(err, "Failed to parse end time")
+
+    draftModel := model.Draft{
+        Owner: model.User{Id: model.GetUserBySessionToken(h.Database, sessionToken.Value)},
+        DisplayName: draftName,
+        Description: description,
+        Interval: intInterval,
+        StartTime: parsedStartTime,
+        EndTime: parsedEndTime,
+        Status: model.GetStatusString(model.FILLING),
+    }
+
+    //TODO we should probably grab the new draft id and then redirecet to that settings page
+    model.CreateDraft(h.Database, &draftModel)
 
     draftCreateIndex := draft.DraftCreateIndex(false, "")
     draftCreate := draft.DraftCreate(" | Create Draft", false, draftCreateIndex)
     //TODO We should probably make tailwind work offline to make the dev experience better
-    err := Render(c, draftCreate)
-    assert.NoErrorCF(err, "Handle View Draft Create Failed To Render")
+    err = Render(c, draftCreate)
+    assert.NoError(err, "Handle View Draft Create Failed To Render")
     return nil
 }
