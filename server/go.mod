4b4aae60dc4a9eb5c2258c2a4cc08e249d1c8c87
squirlemaster42 <jakobmisbach8@gmail.com>
2024-09-06 22:20:57 -0400

    checkpoint

--- server/go.mod
+++ server/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.9.0
+	golang.org/x/crypto v0.21.0
 )
 
 require (
@@ -18,7 +19,6 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
---
+++ server/model/asset.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	"database/sql"
+	"image"
+)
+
+type Asset struct {
+    Id int
+    Path string
+    Asset image.Image //TODO Is this right
+}
+
+//TODO we should have something in the .env file to define the asset path
+type AssetManager struct {
+    AssetPath string
+}
+
+func (a *AssetManager) LoadDraftProfileAsset(database *sql.DB, draftId int) *Asset {
+    return nil
+}
+
+//The actual html will need to base64 encode the image
+
+//TODO Think about what sort of validation we need to do for replacing images (we might not need to do any since its obvious to the user)
+func (a *AssetManager) UploadDraftProfileAsset(database *sql.DB, draftId int, image image.Image) error {
+    //Generate a unique id for the image
+    //assetId := uuid.New()
+
+    //Save the relative path to the database
+
+    //TODO We need to figure out the image type
+
+    //Point the draft asset to this
+
+    return nil
+}
+
+func (a *AssetManager) LoadUserProfileAsset(database *sql.DB, userId int) *Asset {
+    return nil
+}
+
+func (a *AssetManager) UploadUserProfileAsset(database *sql.DB, userId int, image image.Image) error {
+    return nil
+}
