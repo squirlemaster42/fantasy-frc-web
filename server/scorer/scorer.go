1b4de2a3baf57e253ce54120d0bdf16db1cba0c0
squirlemaster42 <jakobmisbach8@gmail.com>
2024-10-30 23:50:59 -0400

    try to make tailwind work offline

--- server/scorer/scorer.go
+++ server/scorer/scorer.go
@@ -390,7 +390,7 @@ func (s *Scorer) RunScorer() {
 
 	go func(s *Scorer) {
 		for {
-            //TODO Need to add something that allows for rescoring all matches on a set interval
+            //TODO Skip scoring if we are not on an event day
             //Get a list of matches to score and
             //Sort matches by id (they are almost sorted, but we need to move finals matches to the end (no they are not, I dont see any corrilation))
             matches := make(map[string][]string)
