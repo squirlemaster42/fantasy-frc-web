8f688c24baa1a92a5a5b05315ce7c87ff61967c1
dustin2036 <dpaq24@gmail.com>
2025-02-27 23:36:06 -0500

    Made logout work

--- server/server.go
+++ server/server.go
@@ -1,51 +1,52 @@
 package main
 
 import (
-	"database/sql"
-	"server/assert"
-	"server/authentication"
-	"server/handler"
-	"server/logging"
-	"server/tbaHandler"
+    "database/sql"
+    "server/assert"
+    "server/authentication"
+    "server/handler"
+    "server/logging"
+    "server/tbaHandler"
 
-	"github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4"
 )
 
 func CreateServer(db *sql.DB, tbaHandler *tbaHandler.TbaHandler, logger *logging.Logger) {
     logger.Log("Starting Server")
-	assert := assert.CreateAssertWithContext("Create Server")
+    assert := assert.CreateAssertWithContext("Create Server")
     auth := authentication.NewAuth(db, logger)
-	app := echo.New()
+    app := echo.New()
     app.IPExtractor = echo.ExtractIPDirect()
-	app.Static("/", "./assets")
+    app.Static("/", "./assets")
 
-	//Setup Routes
-	h := handler.Handler{
-		Database:   db,
-		Logger:     logger,
-		TbaHandler: *tbaHandler,
+    //Setup Routes
+    h := handler.Handler{
+        Database:   db,
+        Logger:     logger,
+        TbaHandler: *tbaHandler,
         Notifier: &handler.PickNotifier{
             Database: db,
             Watchers: make(map[int][]handler.Watcher),
         },
-	}
+    }
     //TODO Make the base route. Something about
     //having this here breaks everything?
     //app.GET("/", h.HandleViewHome)
-	app.GET("/login", h.HandleViewLogin)
-	app.POST("/login", h.HandleLoginPost)
-	app.GET("/register", h.HandleViewRegister)
-	app.POST("/register", h.HandlerRegisterPost)
+    app.GET("/login", h.HandleViewLogin)
+    app.POST("/login", h.HandleLoginPost)
+    app.GET("/register", h.HandleViewRegister)
+    app.POST("/register", h.HandlerRegisterPost)
+    app.POST("/logout", h.HandleLogoutPost)
 
     protected := app.Group("/u", auth.Authenticate)
-	protected.GET("/home", h.HandleViewHome)
-	protected.GET("/createDraft", h.HandleViewCreateDraft)
+    protected.GET("/home", h.HandleViewHome)
+    protected.GET("/createDraft", h.HandleViewCreateDraft)
     protected.POST("/createDraft", h.HandleCreateDraftPost)
-	protected.GET("/draft/:id/profile", h.HandleViewDraftProfile)
-	protected.POST("/draft/updateDraft", h.HandleUpdateDraftProfile)
-	protected.GET("/draft/:id/pick", h.ServePickPage)
-	protected.POST("/draft/:id/makePick", h.HandlerPickRequest)
-	protected.GET("/draft/:id/pickNotifier", h.PickNotifier)
+    protected.GET("/draft/:id/profile", h.HandleViewDraftProfile)
+    protected.POST("/draft/updateDraft", h.HandleUpdateDraftProfile)
+    protected.GET("/draft/:id/pick", h.ServePickPage)
+    protected.POST("/draft/:id/makePick", h.HandlerPickRequest)
+    protected.GET("/draft/:id/pickNotifier", h.PickNotifier)
     protected.POST("/draft/:id/invitePlayer", h.InviteDraftPlayer)
     protected.GET("/team/score", h.HandleTeamScore)
     protected.POST("/team/score", h.HandleGetTeamScore)
@@ -57,6 +58,6 @@ func CreateServer(db *sql.DB, tbaHandler *tbaHandler.TbaHandler, logger *logging
     admin.GET("/console", h.HandleAdminConsoleGet)
     admin.POST("/processCommand", h.HandleRunCommand)
 
-	err := app.Start(":3000")
-	assert.NoError(err, "Failed to start server")
+    err := app.Start(":3000")
+    assert.NoError(err, "Failed to start server")
 }
