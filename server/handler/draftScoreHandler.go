ce4640da3fdbbddf4f8a85be9e82b6304e8c4259
squirlemaster42 <jakobmisbach8@gmail.com>
2025-04-16 20:10:18 -0400

    Sort picks and players by score

--- server/handler/draftScoreHandler.go
+++ server/handler/draftScoreHandler.go
@@ -4,6 +4,7 @@ import (
 	"server/assert"
 	"server/model"
 	"server/view/draft"
+	"slices"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
@@ -21,6 +22,24 @@ func (h *Handler) HandleDraftScore(c echo.Context) error {
 	assert.NoError(err, "Failed to convert draft id to int")
 
 	userDraftScore := model.GetDraftScore(h.Database, draftId)
+
+    //Sort user scores
+    slices.SortFunc(userDraftScore, func(a, b model.DraftPlayer) int {
+        //This will return a negative number when a score is less than b score
+        //a positive number is a score is greater than b score
+        //and 0 if they are equal
+        return a.Score - b.Score
+    })
+
+    for _, draftPlayer := range userDraftScore {
+        //Sort team scores
+        slices.SortFunc(draftPlayer.Picks, func(a, b model.Pick) int {
+            //This will return a negative number when a score is less than b score
+            //a positive number is a score is greater than b score
+            //and 0 if they are equal
+            return a.Score - b.Score
+        })
+    }
 
 	draftIndex := draft.DraftScoreIndex(userDraftScore)
 	draft := draft.DraftScore(" | Draft Score", true, username, draftIndex, draftId)
