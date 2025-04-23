d6126a538aa6e5b0fe3fcbc3d7be2d5642b917ca
squirlemaster42 <jakobmisbach8@gmail.com>
2025-04-23 19:51:15 -0400

    Valid pick in pick manager

--- server/picking/pickManager.go
+++ server/picking/pickManager.go
@@ -1,11 +1,19 @@
 package picking
 
-import "sync"
+import (
+    "database/sql"
+    "errors"
+    "server/model"
+    "server/tbaHandler"
+    "sync"
+)
 
 type PickManager struct {
     draftId int
     lock sync.Mutex
     listeners []*PickListener
+    database *sql.DB
+    tbaHandler *tbaHandler.TbaHandler
 }
 
 type PickEvent struct {
@@ -18,9 +26,10 @@ type PickListener interface {
 
 //TODO Do we want to wrap this in another layer so you
 //cannot create an more than one pick manager per draft
-func NewPickManager(draftId int) *PickManager {
+func NewPickManager(draftId int, database *sql.DB, tbaHandler *tbaHandler.TbaHandler) *PickManager {
     return &PickManager{
         draftId: draftId,
+        database: database,
     }
 }
 
@@ -29,12 +38,16 @@ func (p *PickManager) makePick(pick string) error {
     p.lock.Lock()
     defer p.lock.Unlock()
     //validate pick
-    for _, listener := range p.listeners {
-        (*listener).recievePickEvent(PickEvent{
-            pick: pick,
-        })
+    if model.ValidPick(p.database, p.tbaHandler, pick, p.draftId) {
+        for _, listener := range p.listeners {
+            (*listener).recievePickEvent(PickEvent{
+                pick: pick,
+            })
+        }
+        return nil
+    } else {
+        return errors.New("Invalid Pick")
     }
-    return nil
 }
 
 func (p *PickManager) AddListener(listener PickListener) {
@@ -42,6 +55,4 @@ func (p *PickManager) AddListener(listener PickListener) {
 }
 
 //TODO What should this take in?
-func (p *PickManager) RemoveListener() {
-
-}
+func (p *PickManager) RemoveListener() { }
