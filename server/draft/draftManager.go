b38b2f93a452e4b2d2b37700b4587e842497b85b
squirlemaster42 <jakobmisbach8@gmail.com>
2025-05-02 00:26:34 -0400

    More work to setup state transitions

--- server/draft/draftManager.go
+++ server/draft/draftManager.go
@@ -3,6 +3,7 @@ package draft
 import (
 	"database/sql"
 	"fmt"
+	"server/assert"
 	"server/model"
 	"server/picking"
 	"server/tbaHandler"
@@ -14,12 +15,44 @@ func NewDraftManager(tbaHandler *tbaHandler.TbaHandler, database *sql.DB) *Draft
         drafts: map[int]*Draft{},
         database: database,
         tbaHandler: tbaHandler,
-        states: setupStates(),
+        states: setupStates(database),
     }
 }
 
 type stateTransition interface {
-    executeTransition(previousState model.DraftState, draft Draft)
+    executeTransition(draft *Draft) error
+}
+
+type ToStartTransition struct {
+    database *sql.DB
+}
+
+func (tst *ToStartTransition) executeTransition(draft *Draft) error {
+    return nil
+}
+
+type ToPickingTransition struct {
+    database *sql.DB
+}
+
+func (tpt *ToPickingTransition) executeTransition(draft *Draft) error {
+    return nil
+}
+
+type ToPlayingTransition struct {
+    database *sql.DB
+}
+
+func (tpt *ToPlayingTransition) executeTransition(draft *Draft) error {
+    return nil
+}
+
+type ToCompleteTransition struct {
+    database *sql.DB
+}
+
+func (tct *ToCompleteTransition) executeTransition(draft *Draft) error {
+    return nil
 }
 
 type state struct {
@@ -27,24 +60,36 @@ type state struct {
     transitions map[model.DraftState]stateTransition
 }
 
-func setupStates() map[model.DraftState]*state {
+func setupStates(database *sql.DB) map[model.DraftState]*state {
     states := make(map[model.DraftState]*state)
     states[model.FILLING] = &state {
         state: model.FILLING,
     }
-    states[model.FILLING].transitions[model.WAITING_TO_START] = nil
+    states[model.FILLING].transitions[model.WAITING_TO_START] = &ToStartTransition{
+        database: database,
+    }
+
     states[model.WAITING_TO_START] = &state {
         state: model.WAITING_TO_START,
     }
-    states[model.WAITING_TO_START].transitions[model.PICKING] = nil
+    states[model.WAITING_TO_START].transitions[model.PICKING] = &ToPickingTransition {
+        database: database,
+    }
+
     states[model.PICKING] = &state {
         state: model.PICKING,
     }
-    states[model.PICKING].transitions[model.TEAMS_PLAYING] = nil
+    states[model.PICKING].transitions[model.TEAMS_PLAYING] = &ToPlayingTransition{
+        database: database,
+    }
+
     states[model.TEAMS_PLAYING] = &state {
         state: model.TEAMS_PLAYING,
     }
-    states[model.TEAMS_PLAYING].transitions[model.COMPLETE] = nil
+    states[model.TEAMS_PLAYING].transitions[model.COMPLETE] = &ToCompleteTransition{
+        database: database,
+    }
+
     states[model.COMPLETE] = &state {
         state: model.COMPLETE,
     }
@@ -96,12 +141,24 @@ func (e *invalidStateTransitionError) Error() string {
     return fmt.Sprintf("Invalid state tranition where current state was %s and requested state was %s", e.currentState, e.requestedState)
 }
 
-func (d *Draft) ExecuteDraftStateTransition(requestedState model.DraftState, database *sql.DB) error {
-    d.stateLock.Lock()
-    defer d.stateLock.Unlock()
+func (dm *DraftManager) ExecuteDraftStateTransition(draft *Draft, requestedState model.DraftState, database *sql.DB) error {
+    assert := assert.CreateAssertWithContext("Execute Draft State Transition")
+    assert.AddContext("Draft Id", draft.draftId)
+    assert.AddContext("Current State", string(draft.model.Status))
+    assert.AddContext("Requested State", string(requestedState))
 
-    return &invalidStateTransitionError{
-        currentState: model.FILLING,
-        requestedState: model.FILLING,
+    draft.stateLock.Lock()
+    defer draft.stateLock.Unlock()
+
+    state, stateFound := dm.states[draft.model.Status]
+    assert.RunAssert(stateFound, "Current draft state is not registed in state machine")
+    transition, transitionFound := state.transitions[requestedState]
+    if !transitionFound {
+        return &invalidStateTransitionError{
+            currentState: model.FILLING,
+            requestedState: model.FILLING,
+        }
     }
+
+    return transition.executeTransition(draft)
 }
