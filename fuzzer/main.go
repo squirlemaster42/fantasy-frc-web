ee9095089e396ea142b7844d85a7820e4e8f6d6f
squirlemaster42 <jakobmisbach8@gmail.com>
2025-05-14 00:42:59 -0400

    More fuzzer work

--- fuzzer/main.go
+++ fuzzer/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log/slog"
+	"math/rand"
 	"os"
 	"server/assert"
-    "server/swagger"
+	"server/swagger"
+	"strconv"
+	"strings"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
+
+var validCompLevels []string = []string {
+    "q",
+    "qf",
+    "sf",
+    "f",
+}
 
 func RegisterDatabaseConnection(username string, password string, ip string, dbName string) *sql.DB{
     slog.Info("Setting up DB connection", "User", username, "Ip", ip, "Database Name", dbName)
@@ -20,6 +31,12 @@ func RegisterDatabaseConnection(username string, password string, ip string, dbN
     db, err := sql.Open("postgres", connStr)
     a.NoError(err, "Could not open database connection")
     a.NoError(db.Ping(), "Failed to ping database")
+
+    fmt.Println(setupRandomMatch())
+    fmt.Println(setupRandomMatch())
+    fmt.Println(setupRandomMatch())
+    fmt.Println(setupRandomMatch())
+    fmt.Println(setupRandomMatch())
 
     return db
 }
@@ -42,7 +59,7 @@ func main() {
     createFuzzyMatch(validTeams)
 }
 
-func getValidTeams(database *sql.DB) map[string]bool {
+func getValidTeams(database *sql.DB) []string {
     assert := assert.CreateAssertWithContext("Get Valid Teams")
 
     query := `Select
@@ -53,16 +70,58 @@ func getValidTeams(database *sql.DB) map[string]bool {
     rows, err := stmt.Query()
     assert.NoError(err, "Failed to query valid teams")
 
-    validTeams := make(map[string]bool)
+    var validTeams []string
     for rows.Next() {
         var team string
         rows.Scan(&team)
-        validTeams[team] = true
+        validTeams = append(validTeams, team)
     }
 
     return validTeams
 }
 
-func createFuzzyMatch(validTeams map[string]bool) swagger.Match {
-    return swagger.Match{}
+func createFuzzyMatch(validTeams []string) swagger.Match {
+    match := setupRandomMatch()
+    match.Alliances.Red = createMatchAlliance(validTeams)
+    match.Alliances.Blue = createMatchAlliance(validTeams)
+
+    return match
+}
+
+func createMatchAlliance(validTeams []string) *swagger.MatchAlliance {
+    return nil
+}
+
+const alphabet string = "abcdefghijklmnopqrstuvwxyz"
+
+func setupRandomMatch() swagger.Match {
+    match := swagger.Match{
+        CompLevel: validCompLevels[rand.Intn(len(validCompLevels))],
+        MatchNumber: rand.Int31n(999) + 1,
+    }
+    sb := strings.Builder{}
+    year :=  rand.Intn(3000) + 1
+    sb.WriteString(strconv.Itoa(year))
+    sb.WriteString(getRandomString(rand.Intn(4) + 2, alphabet))
+    sb.WriteString("_")
+    sb.WriteString(match.CompLevel)
+    if match.CompLevel != "q" {
+        match.SetNumber = rand.Int31n(3) + 1
+        sb.WriteString(strconv.Itoa(int(match.SetNumber)))
+    }
+    sb.WriteString("m")
+    sb.WriteString(strconv.Itoa(int(match.MatchNumber)))
+    match.Key = sb.String()
+    return match
+}
+
+func getRandomString(length int, charset string) string {
+    assert := assert.CreateAssertWithContext("Get Random String")
+    assert.AddContext("Length", length)
+    assert.AddContext("Charset", charset)
+    b := make([]byte, length)
+    for i := range b {
+        b[i] = charset[rand.Intn(len(charset))]
+    }
+    return string(b)
 }
