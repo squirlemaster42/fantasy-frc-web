0d7e4c8494aa58abf24b5a0c86d485814f3d70a4
squirlemaster42 <jakobmisbach8@gmail.com>
2025-06-11 23:29:49 -0400

    Rename to uuid

--- database/changeUserIdToGuid.sql
+++ database/changeUserIdToGuid.sql
@@ -1,20 +1,20 @@
 CREATE Extension "uuid_ossp";
 
 -- Alter columns and update data
-Alter Table Users Add Column UserGuid;
-Update Users Set UserGuid = uuid_generate_v4();
-Alter Table UserSessions Add Column UserGuid UUID;
-Update UserSessions us Set UserGuid = u.UserGuid From Users u Where us.UserId = u.Id;
-Alter Table DraftPlayers Add Column UserGuid UUID;
-Update DraftPlayers Set UserGuid = u.UserGuid From Users u Where u.Id = DraftPlayers.Player;
-Alter Table DraftReaders Add Column UserGuid UUID;
-Update DraftReaders dr Set UserGuid = u.UserGuid From Users u Where u.Id = dr.Player;
-Alter Table DraftInvites Add Column InvitedUserGuid UUID;
-Alter Table DraftInvites Add Column InvitingUserGuid UUID;
-Update DraftInvites di Set InvitedUserGuid = u.UserGuid From Users u Where u.Id = di.InvitedPlayer;
-Update DraftInvites di Set InvitingUserGuid = u.UserGuid From Users u Where u.Id = di.InvitingPlayer;
-Alter Table Drafts Add Column OwnerUserGuid UUID;
-Update Drafts d Set OwnerUserGuid = u.UserGuid From Users u Where u.Id = d.Owner;
+Alter Table Users Add Column UserUuid;
+Update Users Set UserUuid = uuid_generate_v4();
+Alter Table UserSessions Add Column UserUuid UUID;
+Update UserSessions us Set UserUuid = u.UserUuid From Users u Where us.UserId = u.Id;
+Alter Table DraftPlayers Add Column UserUuid UUID;
+Update DraftPlayers Set UserUuid = u.UserUuid From Users u Where u.Id = DraftPlayers.Player;
+Alter Table DraftReaders Add Column UserUuid UUID;
+Update DraftReaders dr Set UserUuid = u.UserUuid From Users u Where u.Id = dr.Player;
+Alter Table DraftInvites Add Column InvitedUserUuid UUID;
+Alter Table DraftInvites Add Column InvitingUserUuid UUID;
+Update DraftInvites di Set InvitedUserUuid = u.UserUuid From Users u Where u.Id = di.InvitedPlayer;
+Update DraftInvites di Set InvitingUserUuid = u.UserUuid From Users u Where u.Id = di.InvitingPlayer;
+Alter Table Drafts Add Column OwnerUserUuid UUID;
+Update Drafts d Set OwnerUserUuid = u.UserUuid From Users u Where u.Id = d.Owner;
 
 -- Drop UserId columns
 Alter Table UserSessions Drop UserId;
@@ -26,15 +26,15 @@ Alter Table Drafts Drop Owner;
 
 -- Change PK on Users to GUID
 Alter Table Users Drop Constraint Users_pkey;
-Alter Table Users Add Primary Key (UserGuid);
+Alter Table Users Add Primary Key (UserUuid);
 
 -- Update FK contraints
-Alter Table UserSessions Add Constraint fk_session_user Foreign Key (UserGuid) References Users (UserGuid);
-Alter Table DraftPlayers Add Constraint fk_session_user Foreign Key (UserGuid) References Users (UserGuid);
-Alter Table DraftReaders Add Constraint fk_reader_user Foreign Key (UserGuid) References Users (UserGuid);
-Alter Table DraftInvites Add Constraint fk_invited_user Foreign Key (InvitedUserGuid) References Users (UserGuid);
-Alter Table DraftInvites Add Constraint fk_inviting_user Foreign Key (InvitingUserGuid) References Users (UserGuid);
-Alter Table Drafts Add Constraint fk_owner_user Foreign Key (OwnerUserGuid) References Users (UserGuid);
+Alter Table UserSessions Add Constraint fk_session_user Foreign Key (UserUuid) References Users (UserUuid);
+Alter Table DraftPlayers Add Constraint fk_session_user Foreign Key (UserUuid) References Users (UserUuid);
+Alter Table DraftReaders Add Constraint fk_reader_user Foreign Key (UserUuid) References Users (UserUuid);
+Alter Table DraftInvites Add Constraint fk_invited_user Foreign Key (InvitedUserUuid) References Users (UserUuid);
+Alter Table DraftInvites Add Constraint fk_inviting_user Foreign Key (InvitingUserUuid) References Users (UserUuid);
+Alter Table Drafts Add Constraint fk_owner_user Foreign Key (OwnerUserUuid) References Users (UserUuid);
 
 -- Drop Id on Users
 Alter Table Users Drop Id;
