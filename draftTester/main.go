be9628b114ea78ffd8ac7077983dbb570f9baf63
squirlemaster42 <jakobmisbach8@gmail.com>
2025-08-13 00:03:52 -0400

    Make login work

--- draftTester/main.go
+++ draftTester/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io"
 	"log/slog"
+	"math/rand/v2"
 	"net/http"
 	"net/url"
 	"strings"
@@ -13,8 +15,12 @@ type User struct {
     AuthTok  string
 }
 
+type Draft struct {
+    Id int
+}
+
 const (
-    target = "http://localhost"
+    target = "http://localhost:7331"
 )
 
 func main() {
@@ -57,10 +63,17 @@ func main() {
     }
 
     populateAuthToks(users)
+
+    //Choose a user and create a draft
+}
+
+func createDraft(user *User) Draft {
+    return Draft{}
 }
 
 func populateAuthToks(users map[string]*User) {
     for _, user := range users {
+        slog.Info("Making login request", "User", user.Username)
         form := url.Values{}
         form.Add("username", user.Username)
         form.Add("password", user.Password)
@@ -69,10 +82,22 @@ func populateAuthToks(users map[string]*User) {
             slog.Error("Failed login", "Username", user.Username, "Error", err)
             panic(err)
         }
+
+
         defer resp.Body.Close()
+
+        if resp.StatusCode != 200 {
+            slog.Error("Failed to login", "User", user.Username)
+            panic("failed to login")
+        }
+
+        body, err := io.ReadAll(resp.Body)
+        slog.Info("Request made", "User", user.Username, "Status", resp.StatusCode, "Body", body)
+
         cookies := resp.Cookies()
         for _, cookie := range cookies {
             if cookie.Name == "sessionToken" {
+                slog.Info("Found auth token", "User", user.Username)
                 user.AuthTok = cookie.Value
             }
         }
