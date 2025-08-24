1bbfc393c7e98e541cf98f5255f6ecca28b22390
squirlemaster42 <jakobmisbach8@gmail.com>
2025-08-24 13:55:34 -0400

    Basic landing page

--- server/handler/landingPageHandler.go
+++ server/handler/landingPageHandler.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"server/assert"
+	"server/view"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (h *Handler) HandleViewLanding(c echo.Context) error {
+	assert := assert.CreateAssertWithContext("Handle Landing View")
+
+	landing := view.Landing()
+	err := Render(c, landing)
+	assert.NoError(err, "Handle View Landing Failed To Render")
+	return nil
+}
--- server/server.go
+++ server/server.go
@@ -13,67 +13,62 @@ import (
 )
 
 func CreateServer(serverPort string, h handler.Handler) {
-    slog.Info("Starting Server")
-    assert := assert.CreateAssertWithContext("Create Server")
-    auth := authentication.NewAuth(h.Database)
-    app := echo.New()
-    app.IPExtractor = echo.ExtractIPDirect()
+	slog.Info("Starting Server")
+	assert := assert.CreateAssertWithContext("Create Server")
+	auth := authentication.NewAuth(h.Database)
+	app := echo.New()
+	app.IPExtractor = echo.ExtractIPDirect()
 
-    cacheControlMiddleware := func(next echo.HandlerFunc) echo.HandlerFunc {
-        return func(c echo.Context) error {
-            // Set Cache-Control header (30 days)
-            c.Response().Header().Set("Cache-Control", "public, max-age=2592000")
-            return next(c)
-        }
-    }
+	cacheControlMiddleware := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// Set Cache-Control header (30 days)
+			c.Response().Header().Set("Cache-Control", "public, max-age=2592000")
+			return next(c)
+		}
+	}
 
-    app.Add(
-        http.MethodGet,
-        "/",
-        echo.StaticDirectoryHandler(os.DirFS("./assets"), false),
-        cacheControlMiddleware,
-    )
-    app.Add(
-        http.MethodGet,
-        "/css",
-        echo.StaticDirectoryHandler(os.DirFS("./assets/css"), false),
-        cacheControlMiddleware,
-    )
+	app.Add(
+		http.MethodGet,
+		"/css",
+		echo.StaticDirectoryHandler(os.DirFS("./assets/css"), false),
+		cacheControlMiddleware,
+	)
 
-    app.Use(middleware.Gzip())
-    //app.Use(middleware.Recover())
+	app.Use(middleware.Gzip())
+	//app.Use(middleware.Recover())
 
-    //Setup Routes
-    app.GET("/login", h.HandleViewLogin)
-    app.POST("/login", h.HandleLoginPost)
-    app.GET("/register", h.HandleViewRegister)
-    app.POST("/register", h.HandlerRegisterPost)
-    app.POST("/logout", h.HandleLogoutPost)
-    app.POST("/tbaWebhook", h.ConsumeTbaWebsocket)
+	//Setup Routes
+	app.GET("/", h.HandleViewLanding)
+	app.GET("/login", h.HandleViewLogin)
+	app.POST("/login", h.HandleLoginPost)
+	app.GET("/register", h.HandleViewRegister)
+	app.POST("/register", h.HandlerRegisterPost)
+	app.POST("/logout", h.HandleLogoutPost)
+	app.POST("/tbaWebhook", h.ConsumeTbaWebsocket)
 
-    protected := app.Group("/u", auth.Authenticate)
-    protected.GET("/home", h.HandleViewHome)
-    protected.GET("/createDraft", h.HandleViewCreateDraft)
-    protected.POST("/createDraft", h.HandleCreateDraftPost)
-    protected.GET("/draft/:id/profile", h.HandleViewDraftProfile)
-    protected.POST("/draft/:id/updateDraft", h.HandleUpdateDraftProfile)
-    protected.POST("/draft/:id/startDraft", h.HandleStartDraft)
-    protected.GET("/draft/:id/pick", h.ServePickPage)
-    protected.POST("/draft/:id/makePick", h.HandlerPickRequest)
-    protected.GET("/draft/:id/pickNotifier", h.PickNotifier)
-    protected.POST("/draft/:id/invitePlayer", h.InviteDraftPlayer)
-    protected.GET("/team/score", h.HandleTeamScore)
-    protected.POST("/team/score", h.HandleGetTeamScore)
-    protected.GET("/draft/:id/draftScore", h.HandleDraftScore)
-    protected.POST("/searchPlayers", h.SearchPlayers)
-    protected.GET("/viewInvites", h.HandleViewInvites)
-    protected.POST("/acceptInvite", h.HandleAcceptInvite)
-    protected.POST("/draft/:id/skipPickToggle", h.HandleSkipPickToggle)
+	protected := app.Group("/u", auth.Authenticate)
+	protected.GET("/home", h.HandleViewHome)
+	protected.GET("/createDraft", h.HandleViewCreateDraft)
+	protected.POST("/createDraft", h.HandleCreateDraftPost)
+	protected.GET("/draft/:id/profile", h.HandleViewDraftProfile)
+	protected.POST("/draft/:id/updateDraft", h.HandleUpdateDraftProfile)
+	protected.POST("/draft/:id/startDraft", h.HandleStartDraft)
+	protected.GET("/draft/:id/pick", h.ServePickPage)
+	protected.POST("/draft/:id/makePick", h.HandlerPickRequest)
+	protected.GET("/draft/:id/pickNotifier", h.PickNotifier)
+	protected.POST("/draft/:id/invitePlayer", h.InviteDraftPlayer)
+	protected.GET("/team/score", h.HandleTeamScore)
+	protected.POST("/team/score", h.HandleGetTeamScore)
+	protected.GET("/draft/:id/draftScore", h.HandleDraftScore)
+	protected.POST("/searchPlayers", h.SearchPlayers)
+	protected.GET("/viewInvites", h.HandleViewInvites)
+	protected.POST("/acceptInvite", h.HandleAcceptInvite)
+	protected.POST("/draft/:id/skipPickToggle", h.HandleSkipPickToggle)
 
-    admin := protected.Group("/admin", auth.CheckAdmin)
-    admin.GET("/console", h.HandleAdminConsoleGet)
-    admin.POST("/processCommand", h.HandleRunCommand)
+	admin := protected.Group("/admin", auth.CheckAdmin)
+	admin.GET("/console", h.HandleAdminConsoleGet)
+	admin.POST("/processCommand", h.HandleRunCommand)
 
-    err := app.Start(":" + serverPort)
-    assert.NoError(err, "Failed to start server")
+	err := app.Start(":" + serverPort)
+	assert.NoError(err, "Failed to start server")
 }
