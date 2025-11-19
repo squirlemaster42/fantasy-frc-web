fc1d0fcbed9ee092d579787d0999ae98ce2aad09
squirlemaster42 <jakobmisbach8@gmail.com>
2025-11-19 18:45:02 -0500

    remove todo

--- server/handler/draftProfilePageHandler.go
+++ server/handler/draftProfilePageHandler.go
@@ -191,7 +191,6 @@ func (h *Handler) InviteDraftPlayer(c echo.Context) error {
 func (h *Handler) HandleStartDraft(c echo.Context) error {
 	//TODO we should check that the start time is after the current time. If
 	//not, should we move it to now or throw and error?
-	//TODO we shouldnt be using asserts on the user input here
 	assert := assert.CreateAssertWithContext("Handle Start Draft")
 	userTok, err := c.Cookie("sessionToken")
 	// Session token should always be here because the middleware should have
