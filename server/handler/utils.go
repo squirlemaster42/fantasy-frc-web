b4519fe6bc550e010d7c77cb1d14604dd79ffd91
squirlemaster42 <jakobmisbach8@gmail.com>
2025-11-22 10:45:35 -0500

    feat: correct http status on failed login and fix db setup

--- server/handler/utils.go
+++ server/handler/utils.go
@@ -1,10 +1,20 @@
 package handler
 
 import (
+	"bytes"
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
 
 func Render(c echo.Context, component templ.Component) error {
-    return component.Render(c.Request().Context(), c.Response())
+	return component.Render(c.Request().Context(), c.Response())
+}
+
+func RenderError(c echo.Context, status int, component templ.Component) error {
+	var buf bytes.Buffer
+	err := component.Render(c.Request().Context(), &buf)
+	if err != nil {
+		return err
+	}
+	return c.HTML(status, buf.String())
 }
