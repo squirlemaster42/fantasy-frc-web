4f7dc21085e5d559ae32679e1b0866d5e8503b71
squirlemaster42 <jakobmisbach8@gmail.com>
2026-02-20 16:11:11 -0500

    fix styling

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -4,6 +4,30 @@ module.exports = {
     "./view/**/*.templ",
     "./view/*.templ"
   ],
+  safelist: [
+    'bg-red-500',
+    'bg-orange-500',
+    'bg-amber-500',
+    'bg-yellow-500',
+    'bg-lime-500',
+    'bg-green-500',
+    'bg-emerald-500',
+    'bg-teal-500',
+    'bg-cyan-500',
+    'bg-sky-500',
+    'bg-blue-500',
+    'bg-indigo-500',
+    'bg-violet-500',
+    'bg-purple-500',
+    'bg-fuchsia-500',
+    'bg-pink-500',
+    'bg-rose-500',
+    'bg-slate-900',
+    'bg-slate-800',
+    'bg-slate-700',
+    'bg-slate-600',
+    'bg-slate-400',
+  ],
   theme: {
     extend: {
       fontFamily: {
