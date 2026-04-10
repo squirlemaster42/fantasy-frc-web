d84eba23f6e21c74cbdc0f1c5cfb170ac71e2077
squirlemaster42 <jakobmisbach8@gmail.com>
2026-04-10 19:47:47 -0400

    log streaming

--- server/server.go
+++ server/server.go
@@ -66,6 +66,7 @@ func CreateServer(serverPort string, h handler.Handler, sentryDNS string, metric
 
 	metricAuth := authentication.NewMetricAuth(metricSecret)
 	app.GET("/metrics", echo.WrapHandler(promhttp.Handler()), metricAuth.MetricsAuthMiddleware())
+	app.GET("/logs", h.StreamLogsHandler, metricAuth.MetricsAuthMiddleware())
 
 	protected := app.Group("/u", auth.Authenticate)
 	protected.Use(echomiddleware.Gzip())
