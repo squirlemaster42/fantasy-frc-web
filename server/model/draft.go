dca73ade00f95ac0a0ec27d6b6c8bd708774cac5
squirlemaster42 <jakobmisbach8@gmail.com>
2026-04-30 16:52:44 -0400

    use length instead of hard codeded

--- server/model/draft.go
+++ server/model/draft.go
@@ -1100,7 +1100,7 @@ func NextPick(database *sql.DB, draftId int) DraftPlayer {
 				direction = 1
 			}
 		}
-		if len(picks) % 8 == 0 { //TODO Change to number of picks in draft
+		if len(picks)%len(draft.Players) == 0 {
 			direction = 0
 		}
 		assert.AddContext("Last Player Id", lastPlayer.Id)
