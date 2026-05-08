da0db99c1bd40c71ece5c62bc2895752379c37b9
squirlemaster42 <jakobmisbach8@gmail.com>
2026-05-07 21:02:23 -0400

    think

---
+++ server/draft/draftActor.go
@@ -0,0 +1,10 @@
+package draft
+
+// Message types
+// - State transition
+// - Make pick
+// - Ask (for draft state)
+
+type StateTransitionMessage struct {}
+type PickMessage struct {}
+type DraftStateMessage struct {}
