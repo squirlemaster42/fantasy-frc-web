92605b26c10a90eaad4112c3957059fb65a631f0
PaarkG <2007kdogrules@gmail.com>
2026-07-17 12:14:39 -0400

    update migration

--- database/migrations/00005_draftinvite_status.sql
+++ database/migrations/00005_draftinvite_status.sql
@@ -13,9 +13,26 @@ END;
 ALTER TABLE DraftInvites
     ALTER COLUMN Status SET NOT NULL;
 
--- +goose Down
--- Drop old columns
+-- drop old columns
 
 ALTER TABLE DraftInvites
     DROP COLUMN Accepted,
     DROP COLUMN Canceled;
+
+-- +goose Down
+
+-- add old columns
+ALTER TABLE DraftInvites
+    ADD COLUMN Accepted BOOLEAN,
+    ADD COLUMN Canceled BOOLEAN;
+
+-- set values based on status
+UPDATE DraftInvites SET
+    Accepted = (Status = 'accepted'),
+    Canceled = (Status = 'canceled');
+
+-- remove status
+ALTER TABLE DraftInvites
+    DROP COLUMN Status;
+
+DROP TYPE inviteStatus;
